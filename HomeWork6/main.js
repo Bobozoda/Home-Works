e6b19bb4e498150b66892b2c96c2a50898373b28
Bobozoda Eraj <112476234+Bobozoda@users.noreply.github.com>
2022-11-17 18:09:12 +0500

    HW

---
+++ HomeWork6/main.js
@@ -0,0 +1,59 @@
+// function calculator(a, b, c){
+//      b=="+"? console.log(a+c):(b=="-")?console.log(a-c):(b=="*")?console.log(a*c):(b=="/")?console.log(a/c):console.log();
+// }
+// console.log(calculator(2,"-",6));
+
+// function numArgs(...args){
+//     return args.length
+// }
+// console.log(numArgs(1,2,4));
+
+// function keyboardMistakes(a){
+//     return a.replace("4", "A").replace("5", "S").replace("0", "O").replace("1", "I"); 
+// }
+// console.log(keyboardMistakes("MUB45H1R"));
+
+// function lessThan(a,b){
+//     (a+b<100)?console.log(true):console.log(false);
+// }
+// lessThan(99,3);
+
+
+// function lessThan(a){
+//     return a**3
+// }
+// console.log( lessThan(3));
+
+
+// function lessThan(a,b){
+//     return (a*b)/2
+// }
+// console.log(lessThan(3,2));
+
+// function lessThan(a,b){
+//     (a===b)?console.log(true):console.log(false);
+// }
+// (lessThan(2,2));
+
+
+// function lessThan(a,b){
+//     return b.replace(" ", a)
+// }
+// console.log(lessThan("R", "java "));
+
+
+// function lessThan(a){
+//     let b=a.split(" ");
+//     let min=0
+//     let max=0
+//     max=Math.max(...b)
+//     min=Math.min(...b)
+//     return max+" "+min
+// }
+// console.log(lessThan("1 2 3 4 5"));
+
+
+// function rever(a){
+//     return a.toUpperCase().split('').reverse().join('')
+//   }
+//   console.log(rever("hello"))
